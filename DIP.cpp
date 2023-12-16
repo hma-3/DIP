4bb040f5c4c6deaa295a49f342dbda43047f7cb9
hma-3 <maria.gula0307@gmail.com>
2023-12-16 22:06:00 +0200

    Initial commit

---
+++ DIP.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class DataSource {
+public:
+    virtual string getData() const = 0;
+};
+
+class FileDataSource : public DataSource {
+public:
+    string getData() const override {
+        return "Data from File";
+    }
+};
+
+class ConsoleDataOutput {
+public:
+    void displayData(const DataSource& dataSource) const {
+        cout << "Displaying data: " << dataSource.getData() << endl;
+    }
+};
+
+class DataDisplayer {
+    const DataSource& dataSource;
+    ConsoleDataOutput consoleDataOutput;
+public:
+    DataDisplayer(const DataSource& dataSource) : dataSource(dataSource) {}
+
+    void displayData() const {
+        consoleDataOutput.displayData(dataSource);
+    }
+
+};
+
+int main() {
+    return 0;
+}
